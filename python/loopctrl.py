d7a04a8f48e1e48ecbabde8c0d8eaa855bda5062
Kshitij Datir <kshitijdatir10@gmail.com>
2026-08-21 16:30:00 +0530

    Added new notes

---
+++ python/loopctrl.py
@@ -0,0 +1,32 @@
+#loop control statements = changes loop execution from its normal sequence
+# break used to terminate the loop entirely
+#continue Skips the next iteration of the loop 
+# pass Does nothing acts as a placeholder
+
+#while True:
+    #name=input("Enter your name: ")
+    #if name != "":
+     #   print("Hi "+name)
+      #  break
+    #else:
+     #   print("soryy")
+     #   print("")
+
+
+ # # #this is the use of break statement
+ 
+#phone_number="123-456-789"
+#for i in phone_number:
+#    if i=="-":
+ #       continue
+  #  print(i,end="")
+    
+## here continue is used to skip the dashes in the phone number and to display the only numbers
+
+#for i in range(1,19):
+    #if i == 13:
+       # pass
+    #else:
+       # print(i)
+        
+# Here we used the pass to skip 13 and print all later numbers in the range
