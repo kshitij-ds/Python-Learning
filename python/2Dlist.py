f4502e16dc1a4722e690fd941c153fdc62afb50d
Kshitij Datir <kshitijdatir10@gmail.com>
2026-08-24 19:15:44 +0530

    New learning

---
+++ python/2Dlist.py
@@ -0,0 +1,10 @@
+#2d list = Lists of list
+#drinks=["Coffee", "Tea", "soda"]
+#staters=["pizza","burgers","kebab"]
+#desert=["cake","ice cream"]
+
+#food=[drinks,staters,desert]
+
+#print(food[2][0])  # This gives the exact element of the first list
+#  print(x[list no][element no])
+
