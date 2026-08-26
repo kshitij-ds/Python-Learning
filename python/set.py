da4c5774a8b9f3bac61862258047e4e48e12e934
Kshitij Datir <kshitijdatir10@gmail.com>
2026-08-26 12:52:28 +0530

    New learning

---
+++ python/set.py
@@ -0,0 +1,23 @@
+# Set = Collection which is unordered unindexed , No duplicate value 
+
+utensils={"fork","spoon", "Knife"}
+dishes={"Bowl", 'plate', "Cup",'Knife'}
+    
+# Some functions of sets
+
+#utensils.add("Choping board")  # it adds the desired thing in the set
+
+#utensils.remove("fork") #  It removes the desired thing from the set
+
+#utensils.clear()  It clears the whole set
+
+#utensils.update(dishes)    # we can also use it vica versa
+
+#dineer_table= utensils.union(dishes)  # to print all the elements using a different variable
+
+#print(utensils.difference(dishes))  # It prints what utensils has different from dishes, in this example knife is common in both of them!
+
+#print(utensils.intersection(dishes))  #It prints what both the sets have in common
+
+#for x in dineer_table:
+ #   print(x)
